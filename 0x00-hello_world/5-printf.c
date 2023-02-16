2a7b46dd6e986156106612d3b6db4a28a141cca2
ProxyLinks <adekunlesholabomi@gmail.com>
2023-02-16 13:49:16 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
-*/
+#include <stdio.h>
+/**
+ * main-prints a string followed bt a new line
+ * Return:0
+ */
+
 int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art"\n);
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
