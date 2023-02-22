e6b0351f025d242a8ba7063af3bb0b5685dc92b9
ProxyLinks <adekunlesholabomi@gmail.com>
2023-02-21 20:50:00 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
 
 int main(void)
 {
-	char c[9] = "_putchar";
-	int i = 0;
-	while (i < 8)
-	{
-		_putchar(c[i]);
-		i++;
-	}
-	_putchar('\n');
-	return (0);
+char c[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }
