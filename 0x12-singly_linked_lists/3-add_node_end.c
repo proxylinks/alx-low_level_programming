d9a536bef4232fa4e4d6cb3c03f2e182c372666d
ProxyLinks <adekunlesholabomi@gmainl.com>
2023-03-30 06:18:52 -0700

    singly_linked_list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,43 +1,54 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
-/**
- * add_node_end - Add a new node at the end of a list.
- * @head: Address of the first node of a list.
- * @str: Address of the string to insert into the new node.
- * Return: Address of the new node.
- **/
 
+/**
+ * _strlen - finds the length of a string
+ * @str: string to find the length of
+ *
+ * Return: length of string
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * add_node_end - adds a new node to the end of linked list
+ * @head: double pointer to a linked list
+ * @str: string to add to the new node
+ *
+ * Return: pointer to the new node
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *temp, *temp2;
-	unsigned int length = 0;
+	list_t *new, *tmp;
 
 	if (str == NULL)
 		return (NULL);
-
-	temp = malloc(sizeof(list_t));
-	if (temp == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-
-	temp->str = strdup(str);
-	if (temp->str == NULL)
+	new->str = strdup(str);
+	if (new->str == NULL)
 	{
-		free(temp);
+		free(new);
 		return (NULL);
 	}
-	while (str[length])
-		length++;
-	temp->len = length;
-	temp->next = NULL;
-
+	new->len = _strlen(new->str);
+	new->next = NULL;
 	if (*head == NULL)
 	{
-		*head = temp;
-		return (temp);
+		*head = new;
+		return (new);
 	}
-
-	temp2 = *head;
-	while (temp2->next)
-		temp2 = temp2->next;
-	temp2->next = temp;
-	return (temp);
+	tmp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+	return (new);
 }
