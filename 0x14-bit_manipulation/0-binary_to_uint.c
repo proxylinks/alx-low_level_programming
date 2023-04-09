ae313cd8acc3eea85f9bc7133ad8e60d6eb1acc8
ProxyLinks <adekunlesholabomi@gmainl.com>
2023-04-09 03:05:05 -0700

    bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,6 +4,13 @@
 #include <math.h>
 #include "main.h"
 
+/**
+ * binary_to_uint -converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
+ *
+ * Return:converted number, or 0
+ */
+
 unsigned int binary_to_uint(const char *b) {
     unsigned int decimal_num = 0;
     int len = strlen(b);
